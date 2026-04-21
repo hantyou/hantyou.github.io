09d871f56ca1e534cceaecdc96a87da77d548eb9
hantyou <p.zhai@tudelft.nl>
2026-04-21 13:59:45 +0000

    Deploying to gh-pages from @ hantyou/hantyou.github.io@a1e79c2a615492d729310ba3be0f34a357ea2628 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum vitae of Peiyuan Zhai, PhD researcher at Delft University of Technology, focusing on autonomous perception, sensor fusion, and distributed signal processing.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
